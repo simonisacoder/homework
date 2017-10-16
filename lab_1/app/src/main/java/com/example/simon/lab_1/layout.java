2822a11919bead722470c90d81cc21dfc3b75c32
simonisacoder <921181549@qq.com>
2017-10-17 03:24:15 +0800

    创建master

---
+++ lab_1/app/src/main/java/com/example/simon/lab_1/layout.java
@@ -0,0 +1,20 @@
+package com.example.simon.lab_1;
+
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+    }
+    mImage = (ImageView)findViewById(R.id.image);
+
+
+}
